2410b866e9112bdd9d4522e3147b3d4666355771
Your Name <you@example.com>
2025-01-07 21:30:14 +0200

    Second_Commit

--- lib/src/core/style/color_app.dart
+++ lib/src/core/style/color_app.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
 class ColorsApp {
-  // static const Color kBackgroundColor = Color(0xFFF1EFF1);
-  // static const Color kPrimaryColor = Color(0xFF219cbc);
-  // static const Color kSecondaryColor = Color(0xFFfcca46);
-  // static const Color kTextColor = Color(0xFF023047);
-  // static const Color kTextLightColor = Color(0xFF747474);
-  // static const Color kBlueColor = Color(0xFF488AD5);
   static const Color kBackgroundColor = Color(0xFFF1EFF1);
   static const Color kPrimaryColor = Color(0xFFF1EFF1);
   static const Color kSecondaryColor = Color.fromARGB(255, 74, 39, 136);
